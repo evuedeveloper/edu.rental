37bfc585b5bd145feca55b4fd2a164ee96f6d10a
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-11 19:39:43 +0530

    margin correction Provider.jsx

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -3,9 +3,11 @@ import Header from './_components/Header';
 
 function Provider({children}) {
   return (
-    <div>
-        <Header />
+    <div> 
+      <Header/>
+      <div style={{ marginTop: '9.5rem' }}>
         {children}
+      </div>
     </div>
   )
 }
