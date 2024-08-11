718910fda6f25987f9ec2b4c6ac61c3bcfcd4392
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-11 22:56:56 +0530

    updated Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from '@/components/ui/button';
+import { useUser, UserButton } from '@clerk/nextjs'; // Import UserButton here
 import { Plus } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,6 +9,7 @@ import React from 'react';
 
 function Header() {
     const path = usePathname();
+    const { user, isSignedIn } = useUser();
 
     return (
         <div className='p-6 px-10 flex justify-between items-center shadow-sm fixed top-0 w-full z-10 bg-white'>
@@ -31,15 +33,23 @@ function Header() {
                     </li>
                 </ul>
             </div>
-            <div className='flex gap-2'>
-                <Button className='flex gap-2'>
-                    <Plus className='h-5 w-5' />
-                    Post Your Ad
-                </Button>
-                <Button variant='outline'>
-                    <Link href='/sign-in'>Login</Link>
-                </Button>
-            </div>    
+            <div className='flex gap-2 items-center'>
+                <Link href={'/add-new-listing'}>
+                    <Button className='flex gap-2'>
+                        <Plus className='h-5 w-5' />
+                        Post Your Ad
+                    </Button>
+                </Link>
+                {isSignedIn ? (
+                    <UserButton />
+                ) : (
+                    <Link href={'/sign-in'}>
+                        <Button variant='outline'>
+                            <Link href='/sign-in'>Login</Link>
+                        </Button>
+                    </Link>
+                )}
+            </div>
         </div>
     );
 }
