30f14deabe217bf6a76fef473561771b7685f4bd
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-11 16:21:03 +0530

    debug Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,34 +1,47 @@
-"use client"
+"use client";
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function Header() {
     const path = usePathname();
 
-    useEffect(()=>{
-        console.log(path);
-        
-    },[])
-  return (
-    <div className='p-6 px-10 flex justify-between items-center shadow-sm fixed top-0 w-full z-10 bg-white'>
-        <div className='flex gap-12 items-center'>
-            <Image src={'/stu.rental.svg'} height={80} width={80} alt='Logo'/>
-            <ul className='hidden md:flex gap-10'>
-                <Link href={'/'}><li className={`'hover:text-primary font-medium text-sm cursor-pointer' ${path=='/'&&'text-primary'}`}>For Sell</li></Link>             
-                <li className='hover:text-primary font-medium text-sm cursor-pointer'>For Rent</li>
-                <li className='hover:text-primary font-medium text-sm cursor-pointer'>Agent Finder</li>
-            </ul>
+    return (
+        <div className='p-6 px-10 flex justify-between items-center shadow-sm fixed top-0 w-full z-10 bg-white'>
+            <div className='flex gap-12 items-center'>
+                <Image src='/stu.rental.svg' height={80} width={80} alt='Logo' />
+                <ul className='hidden md:flex gap-10'>
+                    <li>
+                        <Link href='/' className={`hover:text-primary font-medium text-sm cursor-pointer ${path === '/' ? 'text-primary' : ''}`}>
+                            For Sell
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/for-rent' className='hover:text-primary font-medium text-sm cursor-pointer'>
+                            For Rent
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/agent-finder' className='hover:text-primary font-medium text-sm cursor-pointer'>
+                            Agent Finder
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className='flex gap-2'>
+                <Button className='flex gap-2'>
+                    <Plus className='h-5 w-5' />
+                    Post Your Ad
+                </Button>
+                <Button variant='outline'>
+                    <Link href='/sign-in'>Login</Link>
+                </Button>
+            </div>    
         </div>
-        <div className='flex gap-2'>
-            <Button className='flex gap-2'><Plus className='h-5 w-5'/>Post Your Ad</Button>
-            <Button variant='outline'>Login</Button>
-        </div>    
-    </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
