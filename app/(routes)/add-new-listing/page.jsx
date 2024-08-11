95f7a7fcafc5c7c19084b364ba191a6f5dc4e88c
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-11 23:00:38 +0530

    created page.jsx of add-new-listing

---
+++ app/(routes)/add-new-listing/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+function AddNewListing() {
+  return (
+    <div className='p-10 flex flex-col gap-5 items-center justify-center'>
+      <h2 className='font-bold text-2xl'>Add New Listing</h2>
+        <div>
+            <h2 className='text-gray-500'>
+                Enter the address you want to list
+            </h2>
+            <GooglePlacesAutocomplete
+                apiKey="****"
+            />
+        </div>
+    </div>
+  )
+}
+
+export default AddNewListing
